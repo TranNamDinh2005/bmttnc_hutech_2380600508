bc0c19db3c967e815b606295cfdd9d832fc42b7a
TranNamDinh2005 <namdinh20005@gmail.com>
2026-05-11 11:23:12 +0700

    add

--- LAB-01/ex02/ex02_10.py
+++ LAB-01/ex02/ex02_10.py
@@ -1,8 +1,4 @@
-# Sửa lại thành không dấu hoàn toàn
 def dao_nguoc_chuoi(chuoi):
     return chuoi[::-1]
-
-# Sử dụng hàm và in kết quả
 input_string = input("Mời nhập chuỗi cần đảo ngược: ")
-# Đảm bảo tên ở đây giống hệt tên ở trên dòng 1
 print("Chuỗi đảo ngược là:", dao_nguoc_chuoi(input_string))
