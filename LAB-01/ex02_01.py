4cc6cb74efc55ac04c51ea86e6e02e2371558304
TranNamDinh2005 <namdinh20005@gmail.com>
2026-05-11 10:41:33 +0700

    cau 02 bai thuc hanh 02

--- LAB-01/ex02_01.py
+++ LAB-01/ex02_01.py
@@ -1,6 +1,6 @@
-# 1. Nhập dữ liệu (Máy sẽ hiện câu hỏi, bạn gõ tên vào từ bàn phím)
+
 ten = input("Nhập tên của bạn: ")
 tuoi = input("Nhập tuổi của bạn: ")
 
-# 2. In thông điệp (Sử dụng đúng tên biến 'ten' và 'tuoi' đã đặt ở trên)
+
 print("Chào mừng,", ten, "! Bạn", tuoi, "tuổi.")
---
+++ LAB-01/ex02_02.py
@@ -0,0 +1,8 @@
+# Nhập bán kính từ người dùng
+ban_kinh = float(input("Nhập bán kính của hình tròn: "))
+
+# Tính diện tích (Công thức: S = 3.14 * R^2)
+dien_tich = 3.14 * (ban_kinh ** 2)
+
+# In kết quả ra màn hình
+print("Diện tích của hình tròn là:", dien_tich)
